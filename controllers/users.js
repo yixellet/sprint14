2e7d70e13541dfe51e92506290622c5b443de560
Kirill Kotelevsky <yixwellet@gmail.com>
2020-08-19 21:37:34 +0400

    API больше не возвращает хеш пароля

--- controllers/users.js
+++ controllers/users.js
@@ -78,7 +78,7 @@ module.exports.login = (req, res) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key');
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true })
         .end();
     })
